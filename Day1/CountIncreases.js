b6538480a4711cb713bf0799b2349b671c987434
Kris Hamade <slash2223456@gmail.com>
2021-12-02 14:20:23 -0500

    Modified Code for Day 1

--- Day1/CountIncreases.js
+++ Day1/CountIncreases.js
@@ -1,20 +1,23 @@
 var fs = require("fs");
 
-var text = fs.readFileSync("/Users/khamade/Documents/Advent_of_Code_2021/Day1/mattnumbers").toString('utf-8');
+var text = fs.readFileSync("./numbers", "utf8");
 var depthValues = text.split("\n");
 var depthIncreaseCount = 0;
 //console.log(depthValues);
 
 //console.log(depthValues.length);
+
 //console.log(`${depthValues[1]} > ${depthValues[1-1]}` + depthValues[1] > depthValues[1-1]);
 
-depthValues.forEach(index => {
+/*depthValues.forEach((depthValue,index) => {
     if(depthValues[index] > depthValues[index-1]){
         console.log(depthValues[index] + " is greater than depth " + depthValues[index-1]);
         depthIncreaseCount++;
     }
-});
-console.log(depthIncreaseCount)
-//depthValues.forEach(index => {depthCount = (depthValues[index] > depthValues[index-1]) ? depthIncreaseCount++ : console.log(depthIncreaseCount);});
+});*/
+
+depthValues.forEach((depthValue,index) => {depthCount = (depthValues[index] > depthValues[index-1]) ? depthIncreaseCount += 1 : console.log(depthIncreaseCount);});
+
+console.log(depthIncreaseCount);
 
 
--- Day1/CountIncreases.ps1
+++ Day1/CountIncreases.ps1
@@ -1,13 +1,13 @@
-$arrayOfDepths = @(Get-Content Day1/numbers)
+$arrayOfDepths = @(Get-Content ..\numbers)
 #$arrayOfDepths = 199, 200, 208, 210, 200, 207, 240, 269, 260, 263
 $depthIndex = 0
 $increasesInDepth = 0
 foreach ($item in $arrayOfDepths) {
-    $depthIndex++
+    $depthIndex += 1
     if ($arrayOfDepths[$depthIndex] -gt $arrayOfDepths[$depthIndex - 1]) {
         #Write-Output $arrayOfDepths[$depthIndex] "is greater than the last"
         Write-Output $arrayOfDepths[$depthIndex] " is greater than"  $arrayOfDepths[$depthIndex-1]
-        $increasesInDepth++
+        $increasesInDepth += 1
     }
 }
 Write-Output $increasesInDepth "increases in depth"
