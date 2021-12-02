eb9c84da1b7b9692c187b300e86e34b68def29e3
Kris Hamade <slash2223456@gmail.com>
2021-12-02 14:20:45 -0500

    Cleaned up Day 1 Javascript

--- Day1/CountIncreases.js
+++ Day1/CountIncreases.js
@@ -3,18 +3,6 @@ var fs = require("fs");
 var text = fs.readFileSync("./numbers", "utf8");
 var depthValues = text.split("\n");
 var depthIncreaseCount = 0;
-//console.log(depthValues);
-
-//console.log(depthValues.length);
-
-//console.log(`${depthValues[1]} > ${depthValues[1-1]}` + depthValues[1] > depthValues[1-1]);
-
-/*depthValues.forEach((depthValue,index) => {
-    if(depthValues[index] > depthValues[index-1]){
-        console.log(depthValues[index] + " is greater than depth " + depthValues[index-1]);
-        depthIncreaseCount++;
-    }
-});*/
 
 depthValues.forEach((depthValue,index) => {depthCount = (depthValues[index] > depthValues[index-1]) ? depthIncreaseCount += 1 : console.log(depthIncreaseCount);});
 
