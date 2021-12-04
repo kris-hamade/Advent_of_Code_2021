0b0f21c0b21acee0fe59ca3dadaac71be2d741a3
Kris Hamade <slash2223456@gmail.com>
2021-12-04 10:37:26 -0500

    Fixed bug with math in depth calculation
    had to change out horizontal position for x[1] because it needs to multiply by the forward command value not the calculated horizontal position

--- Day2/app.js
+++ Day2/app.js
@@ -1,13 +1,11 @@
 var fs = require("fs");
 
-var input = fs.readFileSync("./Day2/examplecommands", "utf8");
+var input = fs.readFileSync("./Day2/inputday2", "utf8");
 var commands = input.split("\n").map(x => {
     direction = x.split(" ", 1)[0]
     distance = parseInt(x.split(" ", 2)[1])
     return [direction,distance]
 });
-
-//console.log(commands);
 
 var horizontalPosition = 0;
 var depth = 0;
@@ -16,8 +14,8 @@ var position = [horizontalPosition, depth];
 
 commands.map(x => {
     x[0] === 'forward' ? horizontalPosition += x[1] : horizontalPosition;
-    x[0] === 'up' ? depth += x[1] : depth;
-    x[0] === 'down' ? depth -= x[1] : depth;
+    x[0] === 'up' ? depth -= x[1] : depth;
+    x[0] === 'down' ? depth += x[1] : depth;
     position = [horizontalPosition, depth];
     return position;
 });
@@ -25,3 +23,4 @@ commands.map(x => {
 console.log("Horizontal Position is: " + horizontalPosition);
 console.log("Depth is: " + depth);
 console.log("Position is: " + position);
+console.log("Puzzle Answer is: " + horizontalPosition * depth);
---
+++ Day2/appPart2.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+
+var input = fs.readFileSync("./Day2/examplecommands", "utf8");
+var commands = input.split("\n").map(x => {
+    direction = x.split(" ", 1)[0]
+    distance = parseInt(x.split(" ", 2)[1])
+    return [direction,distance]
+});
+var horizontalPosition = 0;
+var aim = 0;
+var depth = 0;
+
+commands.map(x => {
+    console.log("command is " + x[0] + " by " + x[1]);
+
+    x[0] === 'forward' ? horizontalPosition += x[1] : horizontalPosition;
+    x[0] === 'up' ? aim -= x[1] : aim;
+    x[0] === 'down' ? aim += x[1] : aim;
+    x[0] === 'forward' ?  depth += (x[1] * aim): depth;
+
+    x[0] === 'forward' ? console.log(`${depth} += ${x[1]} * ${aim}`) : "Depth Does Not Change From This Command";
+    
+    position = [horizontalPosition, depth];
+
+    
+    console.log("Horizontal Position is " + horizontalPosition);
+    console.log("Aim is " + aim);
+    console.log("Depth is: " + depth);
+
+    return depth;
+});
+
+console.log("Horizontal Position is: " + horizontalPosition);
+console.log("Aim is: " + aim);
+console.log("Depth is: " + depth);
+console.log("Puzzle Answer is: " + horizontalPosition * depth);
