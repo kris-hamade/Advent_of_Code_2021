755bd49b4d3ac6eccd61c17cb0f405007cb0b58c
Kris Hamade <slash2223456@gmail.com>
2021-12-04 01:06:17 -0500

    Day 2 Part 1 is Working

--- Day2/app.js
+++ Day2/app.js
@@ -1,7 +1,27 @@
 var fs = require("fs");
 
-var text = fs.readFileSync("./Day2/examplecommands", "utf8");
-var commands = text.split("\n"); 
-//depthValues = depthValues.map(x => parseInt(x));
-split = commands.map(x => x.split(" "));
-console.log(split);
+var input = fs.readFileSync("./Day2/examplecommands", "utf8");
+var commands = input.split("\n").map(x => {
+    direction = x.split(" ", 1)[0]
+    distance = parseInt(x.split(" ", 2)[1])
+    return [direction,distance]
+});
+
+//console.log(commands);
+
+var horizontalPosition = 0;
+var depth = 0;
+
+var position = [horizontalPosition, depth];
+
+commands.map(x => {
+    x[0] === 'forward' ? horizontalPosition += x[1] : horizontalPosition;
+    x[0] === 'up' ? depth += x[1] : depth;
+    x[0] === 'down' ? depth -= x[1] : depth;
+    position = [horizontalPosition, depth];
+    return position;
+});
+
+console.log("Horizontal Position is: " + horizontalPosition);
+console.log("Depth is: " + depth);
+console.log("Position is: " + position);
